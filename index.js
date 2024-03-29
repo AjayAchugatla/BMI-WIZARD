a9a73b40229ed218b2b1ccbdbe893cd81a14bc66
AjayAchugatla <ajayachugatla17@gmail.com>
2024-03-29 14:42:51 +0530

    50

--- index.js
+++ index.js
@@ -1,92 +1,102 @@
-document.querySelector(".checkbtn").addEventListener("click",()=>{
+document.querySelector(".checkbtn").addEventListener("click", () => {
     document.querySelector("nav ul").classList.toggle("block");
     document.querySelector("main").classList.toggle("none");
+    document.querySelector(".main").classList.toggle("none");
 })
 
-document.querySelector(".input").addEventListener("click",()=>{
+document.querySelector(".input").addEventListener("click", () => {
     document.querySelector("nav").classList.toggle("black");
-})
-
-document.querySelector(".input").addEventListener("click",()=>{
     document.querySelector("main").classList.toggle("black");
+    document.querySelector(".main").classList.toggle("black");
 })
 
-var c1=0,c2=0;
-document.querySelectorAll(".units")[0].addEventListener("click",(e)=>{
+var c1 = 0, c2 = 0;
+document.querySelectorAll(".units")[0].addEventListener("click", (e) => {
     e.preventDefault();
     document.querySelector("#lbs").classList.toggle("none");
     document.querySelector("#kgs").classList.toggle("none");
     let state = document.querySelectorAll(".units")[0];
-    if(c1%2===0){
+    if (c1 % 2 === 0) {
         state.innerText = "Swicth to kgs";
     }
-    else{
+    else {
         state.innerText = "Swicth to lbs";
     }
     c1++;
 })
 
-
-document.querySelectorAll(".units")[1].addEventListener("click",(e)=>{
+document.querySelectorAll(".units")[1].addEventListener("click", (e) => {
     e.preventDefault();
     document.querySelector("#cms").classList.toggle("none");
     document.querySelector("#fts").classList.toggle("none");
     document.querySelector("#ins").classList.toggle("none");
     let state = document.querySelectorAll(".units")[1];
-    if(c2%2===0){
+    if (c2 % 2 === 0) {
         state.innerText = "Swicth to cms";
     }
-    else{
+    else {
         state.innerText = "Swicth to fts";
     }
     c2++;
 })
 
 let gen = "";
-document.querySelector("#male").addEventListener("click",(e)=>{
-    gen = (e.target.id);    
+document.querySelector("#male").addEventListener("click", (e) => {
+    gen = (e.target.id);
 })
 
-document.querySelector("#female").addEventListener("click",(e)=>{
-    gen = (e.target.id);    
+document.querySelector("#female").addEventListener("click", (e) => {
+    gen = (e.target.id);
 })
 
-
-
-document.querySelector(".cta").addEventListener("click",(e)=>{
-    // e.preventDefault();
-
-    if(gen==="")
-    alert("Select gender");
-    let w=0,fts=0,ins=0,cms=0,age=0,val=0;
+document.querySelector(".cta").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (gen === "")
+        alert("Select gender");
+    let w = 0, fts = 0, ins = 0, cms = 0, age = 0, val = 0;
     age = document.querySelector("#age").value;
 
-    if(c1%2===0){
+    if (c1 % 2 === 0) {
         val = document.querySelector("#kgs").value;
     }
-    else{
+    else {
         val = document.querySelector("#lbs").value;
         val = Math.round(val * 0.4535);
     }
-    if(c2%2===0){
+    if (c2 % 2 === 0) {
         cms = (document.querySelector("#cms").value);
-        cms = cms /100;
+        cms = cms / 100;
     }
-    else{
+    else {
         fts = (document.querySelector("#fts").value);
         ins = (document.querySelector("#ins").value);
-        cms = Math.round((fts*30.48) + (ins*2.54));
-        cms = cms/100;
+        cms = Math.round((fts * 30.48) + (ins * 2.54));
+        cms = cms / 100;
     }
 
-    if(age==="" || isNaN(cms) || isNaN(val) || cms==0 || val ==0){
+    if (age === "" || isNaN(cms) || isNaN(val) || cms == 0 || val == 0) {
+        e.preventDefault();
         alert("Enter all values properly");
     }
-    else{
-        let res = (val)/(cms*cms);
-        console.log(Math.floor(res));
+    else {
+        document.querySelector("main").style.display = "none";
+        document.querySelector(".main").classList.remove("none");
+        document.querySelector(".main").style.display = "flex";
+        let res = (val) / (cms * cms);
+        document.querySelector(".val").innerText = Math.round(res);
+        if (res < 18.5) {
+            document.querySelector(".val").style.color = "yellow";
+        }
+        else if (res >= 18.5 && res < 25) {
+            document.querySelector(".val").style.color = "green";
+        }
+        else if (res >= 25 && res <= 30) {
+            document.querySelector(".val").style.color = "orange";
+        }
+        else {
+            document.querySelector(".val").style.color = "red";
+        }
     }
-    
-})
 
+})
 
